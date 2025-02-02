45c05b936e0357e60180176883ba2475ea6300b1
Fares Laadjel <laadj-fares@hotmail.com>
2025-02-02 06:48:42 -0500

    Update constants

--- client/src/components/game/constants.js
+++ client/src/components/game/constants.js
@@ -1,9 +1,9 @@
 export const PLAYER_SIZE = 110;
 export const MAX_HEALTH = 100;
-export const PLAYER_SPEED = 1.0;
+export const PLAYER_SPEED = 1.5;
 export const ENEMY_SIZE = 60;
 export const ENEMY_HITBOX_SIZE = 30;
-export const ENEMY_SPEED = 0.4;
+export const ENEMY_SPEED = 1.0;
 export const COLLISION_DAMAGE = 10;
 export const INVINCIBILITY_FRAMES = 1000;
 export const HEALTH_REGEN_RATE = 0.05;
